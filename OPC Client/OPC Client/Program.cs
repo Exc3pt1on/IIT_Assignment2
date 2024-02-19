feac07b2f9273efcf6455763633e9491f9897a9e
Kim Langvannskås <Kimlangv@hotmail.com>
2024-02-19 18:40:24 +0100

    adding C# programs

---
+++ OPC Client/OPC Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using Opc.UaFx.Client;
+using System.Threading;
+
+using (var client = new OpcClient("opc.tcp://localhost:4841"))
+{
+    client.Connect();
+    while (true)
+    {
+        var temperature = client.ReadNode("ns=2;s=Temperature");
+        Console.WriteLine("Current Temperature is {0} °C", temperature);
+        Thread.Sleep(1000);
+    }
+    
+}
---
+++ OPC UA Server/OPC UA Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using Opc.UaFx;
+using Opc.UaFx.Server;
+
+var temperatureNode = new OpcDataVariableNode<double>("Temperature", 100.0);
+
+using (var server = new OpcServer("opc.tcp://localhost:4841/", temperatureNode))
+{
+    server.Start();
+
+    while (true)
+    {
+        if (temperatureNode.Value == 110)
+            temperatureNode.Value = 100;
+        else
+            temperatureNode.Value++;
+
+        temperatureNode.ApplyChanges(server.SystemContext);
+        Thread.Sleep(1000);
+    }
+}
